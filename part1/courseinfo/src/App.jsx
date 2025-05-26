542adc8a57f8806771e79b75592287bd59d069db
Pere Puig Solanilla <perepuigsolanilla@gmail.com>
2025-05-26 07:48:07 +0200

    Initialize project part1 courseinfo.

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,24 @@
+const Header = ({ course }) => <h1>{course}</h1>
+const Part = ({ part: { name, exercises } }) => <li><p>{name} - {exercises} exercices </p></li>
+const Content = ({ parts }) => <ul>{parts.map(part => <Part part={part} key={part.name + part.exercises} />)}</ul>
+const Total = ({ totalExercises }) => <p>Number of exercises {totalExercises} </p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
+  const totalExercises = parts.reduce((acc, part) => acc + part.exercises, 0)
+
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total totalExercises={totalExercises} />
+    </>
+  )
+}
+
+export default App
