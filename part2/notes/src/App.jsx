389952ac6a6de04833900edd42c3868104c9f905
Pere Puig Solanilla <perepuigsolanilla@gmail.com>
2025-06-01 09:51:35 +0200

    part2 c - adding axios and json server to get notes from server side database

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addNote = (event) => {
     event.preventDefault()
