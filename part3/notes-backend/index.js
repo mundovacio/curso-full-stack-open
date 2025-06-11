feea4cafb0c1a80832bbe2f94f860f420953a6f5
Pere Puig Solanilla <perepuigsolanilla@gmail.com>
2025-06-11 07:14:33 +0200

    Add server proxy configuration and update package.json scripts

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3001
 
@@ -9,7 +8,6 @@ morgan.token('body', function getBody(req) {
 })
 
 app.use(morgan('Method :method \nPath :url \nStatus :status \nBody :body \nSize :res[content-length] - :response-time ms \n------'))
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
