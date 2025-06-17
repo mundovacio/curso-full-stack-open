e7479718f74c9c91d2cece026a74ed18f8ed803d
Pere Puig Solanilla <perepuigsolanilla@gmail.com>
2025-06-17 08:27:19 +0200

    feat: implement MongoDB integration for note management

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
+const Note = require('./models/note')
 
 morgan.token('body', function getBody(req) {
   return JSON.stringify(req.body)
@@ -11,82 +13,51 @@ app.use(morgan('Method :method \nPath :url \nStatus :status \nBody :body \nSize 
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  })
 })
 
 app.put('/api/notes/:id', (req, res) => {
   const id = req.params.id
-  notes = notes.map(note => note.id === id ? {...note, ...req.body} : note)
+  notes = notes.map(note => note.id === id ? { ...note, ...req.body } : note)
   const updatedNote = notes.find(note => note.id === id)
   res.json(updatedNote)
 })
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
