60c0ffecf16ac6d2edf0815786f416890ab4b5dc
Pere Puig Solanilla <perepuigsolanilla@gmail.com>
2025-06-19 08:39:22 +0200

    refactor: improve middleware organization and error handling in notes API

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -5,13 +5,13 @@ const morgan = require('morgan')
 const PORT = process.env.PORT
 const Note = require('./models/note')
 
-morgan.token('body', function getBody(req) {
-  return JSON.stringify(req.body)
-})
-
-app.use(morgan('Method :method \nPath :url \nStatus :status \nBody :body \nSize :res[content-length] - :response-time ms \n------'))
-app.use(express.json())
+// middlewares
+morgan.token('body', (req) => JSON.stringify(req.body, null, 4))
+const requestLogger = morgan('Method :method \nPath :url \nStatus :status \nBody :body \nSize :res[content-length] - :response-time ms \n------')
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -23,10 +23,16 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(next)
 })
 
 app.post('/api/notes', (request, response) => {
@@ -46,18 +52,53 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  Note.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end()
-  })
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (req, res) => {
-  const id = req.params.id
-  notes = notes.map(note => note.id === id ? { ...note, ...req.body } : note)
-  const updatedNote = notes.find(note => note.id === id)
-  res.json(updatedNote)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
+
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
